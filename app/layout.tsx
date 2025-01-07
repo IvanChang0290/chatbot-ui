a1cfd52e638465110d59ce91e3571911ae5764a8
Ivan Chang <ivanty2chang@gmail.com>
2025-01-07 14:22:37 +0800

    remove i18n

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import { Toaster } from "@/components/ui/sonner"
 import { GlobalState } from "@/components/utility/global-state"
 import { Providers } from "@/components/utility/providers"
-import TranslationsProvider from "@/components/utility/translations-provider"
-import initTranslations from "@/lib/i18n"
 import { Database } from "@/supabase/types"
 import { createServerClient } from "@supabase/ssr"
 import { Metadata, Viewport } from "next"
@@ -19,9 +17,6 @@ const APP_DESCRIPTION = "Chabot UI PWA!"
 
 interface RootLayoutProps {
   children: ReactNode
-  params: {
-    locale: string
-  }
 }
 
 export const metadata: Metadata = {
@@ -36,7 +31,6 @@ export const metadata: Metadata = {
     capable: true,
     statusBarStyle: "black",
     title: APP_DEFAULT_TITLE
-    // startUpImage: [],
   },
   formatDetection: {
     telephone: false
@@ -64,11 +58,8 @@ export const viewport: Viewport = {
   themeColor: "#000000"
 }
 
-const i18nNamespaces = ["translation"]
-
 export default async function RootLayout({
-  children,
-  params: { locale }
+  children
 }: RootLayoutProps) {
   const cookieStore = cookies()
   const supabase = createServerClient<Database>(
@@ -84,22 +75,14 @@ export default async function RootLayout({
   )
   const session = (await supabase.auth.getSession()).data.session
 
-  const { t, resources } = await initTranslations(locale, i18nNamespaces)
-
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers attribute="class" defaultTheme="dark">
-          <TranslationsProvider
-            namespaces={i18nNamespaces}
-            locale={locale}
-            resources={resources}
-          >
-            <Toaster richColors position="top-center" duration={3000} />
-            <div className="bg-background text-foreground flex h-dvh flex-col items-center overflow-x-auto">
-              {session ? <GlobalState>{children}</GlobalState> : children}
-            </div>
-          </TranslationsProvider>
+          <Toaster richColors position="top-center" duration={3000} />
+          <div className="bg-background text-foreground flex h-dvh flex-col items-center overflow-x-auto">
+            {session ? <GlobalState>{children}</GlobalState> : children}
+          </div>
         </Providers>
       </body>
     </html>
